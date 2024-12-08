b134d0927e424a15fc77cd02af3e785f4d8bd64c
MitVinay <vm05011996@gmail.com>
2024-12-08 21:01:35 +1100

    Done

--- data.py
+++ data.py
@@ -5,7 +5,7 @@ import mlflow
 mlflow.set_tracking_uri("https://dagshub.com/MitVinay/mlops-part2.mlflow")
 dagshub.init(repo_owner='MitVinay', repo_name='mlops-part2', mlflow=True)
 
-mlflow.set_experiment("test1")
+mlflow.set_experiment("test4")
 # hash
 with mlflow.start_run(experiment_id=1):
   mlflow.log_param('parameter name', 'value')
